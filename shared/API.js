9f8426e608be89842486d2bf5ccbef0cd108d1e0
Stuhlmuller Monica <95758678+MonicaSth@users.noreply.github.com>
2022-12-15 14:14:45 +0200

    Update API.js

--- shared/API.js
+++ shared/API.js
@@ -7,8 +7,4 @@ export const searchMovies = (query) => {
   return axios.get(URL);
 };
 
-// TODO: implement this at some point. Currently not used!
-// export const searchActors = (query) => {
-//   const URL = `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&query=${query}&language=en-US`;
-//   return axios.get(URL);
-// };
+
